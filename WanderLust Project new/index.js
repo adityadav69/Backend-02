010281386197162e31942f8a7fd657e20a267b82
Aditya yadav <adityayadaw42@gmail.com>
2025-08-02 12:24:59 +0530

    Build Project

--- WanderLust Project new/index.js
+++ WanderLust Project new/index.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing")
 const path = require("path");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync")
+const ExpressError = require("./utils/ExpressError")
+const listingSchema = require("./schema");
 
 
 const port = 3000;
@@ -32,17 +35,27 @@ async function main() {
     mongoose.connect(MONGO_URL)
 }
 
-//ROUTES
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
+}
 
+//ROUTES
 
 app.get("/", (req, res) => {
     res.send("Root working..")
 })
 
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     let allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-})
+}))
 
 //New Listings
 
@@ -50,50 +63,59 @@ app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 })
 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
 
-})
+}))
 
 //Add new Listing POST Request Handle
 
-app.post("/listings", async (req, res) => {
-    let { listing } = req.body;
-    const newListing = new Listing(listing);
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-})
+
+}))
 
 //Edit Form Serve Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-})
+}))
 
 //Put request handling to update listing Route
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+
     let { id } = req.params;
     let { listing } = req.body;
     let updatedDocument = await Listing.findByIdAndUpdate(id, listing);
     res.redirect(`/listings/${id}`);
-})
+}))
 
 //Delete the listing
 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}))
+
+app.get(/.*/, (req, res) => {
+    throw new ExpressError(404, "Page Not Found");
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Somthing Went Wrond" } = err;
+    res.status(statusCode).render("listings/error", { err });
 })
-
-
 
 //SERVER LISTEN
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 })
+
--- practice/New folder (3)/index.js
+++ practice/New folder (3)/index.js
@@ -1,78 +1,114 @@
-const express=require('express');
+const express = require('express');
 const { default: mongoose } = require('mongoose');
-const app=express()
-const Chat=require('./models/chat');
+const app = express()
+const Chat = require('./models/chat');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const port=3000;
+const port = 3000;
 const methodOverride = require('method-override');
+const ExpressError = require("./ExpressError");
 
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")))
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 main()
-.then((result)=>{
-    console.log("DB Connection Successfull")
-})
-.catch((err)=>{
-    console.log("DB Conection failed")
-})
+    .then((result) => {
+        console.log("DB Connection Successfull")
+    })
+    .catch((err) => {
+        console.log("DB Conection failed")
+    })
 
-async function  main() {
+async function main() {
     mongoose.connect("mongodb://localhost:27017/cukdb")
 }
 
 //Routes
 
-app.get("/listings",async(req,res)=>{
-    let chats=await Chat.find({});
-    res.render("home.ejs",{chats})
+app.get("/listings", async (req, res) => {
+    try {
+        let chats = await Chat.find({});
+        res.render("home.ejs", { chats })
+    } catch (err) {
+        next(err);
+    }
+
 })
 
-app.get("/listings/new",(req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("new.ejs");
 })
 
-app.post("/listings",(req,res)=>{
-    let {by,message,to}=req.body;
-    console.log(by,message,to)
-    Chat.insertOne({by,message,to})
-    .then((result)=>{
-        console.log("Chat inserted successfully",result)
+app.post("/listings", (req, res) => {
+    try {
+        let { by, message, to } = req.body;
+        console.log(by, message, to)
+        Chat.insertOne({ by, message, to })
+            .then((result) => {
+                console.log("Chat inserted successfully", result)
+                res.redirect("/listings")
+            })
+            .catch((err) => {
+                console.log("Chat insertion failed due to ", err);
+                res.send("Please! Provide Correct details to Send the Message");
+            })
+    } catch (error) {
+        next(error)
+    }
+
+})
+
+app.get("/listings/edit/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+        let chat = await Chat.findById(id);
+        if (!chat) {
+            throw new ExpressError(400, "This is asynchronus error");
+
+        }
+        res.render("edit.ejs", { chat })
+    } catch (error) {
+        next(error);
+    }
+
+
+})
+
+app.put("/listings/:id", async (req, res) => {
+    try {
+        let { msg } = req.body;
+        let { id } = req.params;
+        let updated = await Chat.findByIdAndUpdate(id, { message: msg }, { runValidator: true, new: true });
+        console.log(updated)
         res.redirect("/listings")
-    })
-    .catch((err)=>{
-        console.log("Chat insertion failed due to ",err);
-        res.send("Please! Provide Correct details to Send the Message");
-    })
+    } catch (error) {
+        next(error);
+    }
+
 })
 
-app.get("/listings/edit/:id",async(req,res)=>{
-    let {id}=req.params;
-    let chat=await Chat.findById(id);
-    res.render("edit.ejs",{chat})
+app.delete("/listings/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(deletedChat)
+        res.redirect("/listings");
+    } catch (error) {
+        next(error);
+    }
+
 })
 
-app.put("/listings/:id",async(req,res)=>{
-    let {msg}=req.body;
-    let {id}=req.params;
-    let updated=await Chat.findByIdAndUpdate(id,{message:msg},{runValidator:true, new:true});
-    console.log(updated)
-    res.redirect("/listings")
+app.use((err, req, res, next) => {
+    console.log("Error handle")
+    let { status, message } = err;
+    res.status(status).send(message);
 })
 
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    let deletedChat=await Chat.findByIdAndDelete(id);
-    console.log(deletedChat)
-    res.redirect("/listings");
-})
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
